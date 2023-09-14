495346ba327e7c37943cf46bc5937f3c1553170f
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-09-13 21:17:54 -0400

    Variables, tipos y parseInt

--- resources/scripts/mis_scripts.js
+++ resources/scripts/mis_scripts.js
@@ -1,4 +1,30 @@
-alert('Hola mundo!');
-prompt('Ingrese su nombre');
+//alert('Hola mundo!');
+nombre = prompt('Ingrese su nombre');
 
 nom = 'Ana';
+
+// Tipos: numéricos, texto, booleanos, arreglos
+// Numeros: naturales, enteros, reales, racionales, irracionales, complejos
+
+n = 500000;
+nom = 'Pepe';
+// String - Cadena
+r = "nom";
+
+nombre = 'Buenas noches ' + nombre;
+
+alert(nombre);
+
+n = parseInt( prompt('Ingrese un numero'));
+m = parseInt(prompt('Ingrese otro numero'));
+/*
+rescatar valor ingresado por usuario -> '5'
+convertir cadena a entero -> 5
+asignar valor a n
+*/
+resultado = n + m;
+alert('La suma es: ' + resultado);
+
+//TODO: Estructurtas de control (condicionales y bucles)
+
+
