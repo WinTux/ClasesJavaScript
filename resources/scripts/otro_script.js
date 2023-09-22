8d05402fbb5da309f7f8a403054d567aee33a074
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-09-21 21:27:21 -0400

    funciones

--- resources/scripts/otro_script.js
+++ resources/scripts/otro_script.js
@@ -3,7 +3,7 @@
 /* FUNCIONES */
 
 //Propias de JavaScript
-
+//sobre cadenas
 nom = 'Pepe';
 console.log(nom.length); 
 ape = ' Peralta';
@@ -34,6 +34,58 @@ nombre = 'Samantha Rocha Loza';
 palabras = nombre.split(' ');
 console.log(palabras);
 
-
+ape = '    Peralta Perales         ';
 console.log('->' + ape + '<-');
 console.log('->' + ape.trim() + '<-');
+
+//sobre numeros
+num1 = 0;
+num2 = 0;
+console.log(num1/num2);
+
+if(isNaN(num1/num2))
+    console.log('División entre 0 detectada');
+else
+    console.log(num1/num2);
+
+    num1 = 10;
+    num2 = 0;
+    console.log(num1/num2);
+    num1 = -10;
+    num2 = 0;
+    console.log(num1/num2);
+    num1 = 10;
+    num2 = 3;
+    res = num1/num2;
+    console.log(res.toFixed(2));
+
+/*
+    FUNCIONES PROPIAS DE NOSOTROS
+*/
+
+function sumar(){
+    a = parseInt(prompt('Ingrese un numero'));
+    b = parseInt(prompt('Ingrese otro numero'));
+    r = a + b;
+    console.log('La suma es: ' + r);
+}
+
+opt = prompt('¿Desea realizar una suma? (SI/NO)');
+while(opt == 'SI'){
+    sumar();
+    opt = prompt('¿Desea realizar otra suma? (SI/NO)');
+}
+
+function restar(numero_a, numero_b){
+    resultado = numero_a - numero_b;
+    console.log('La esta es: ' + resultado);
+}
+
+opt = prompt('¿Desea realizar una resta? (SI/NO)');
+while(opt == 'SI'){
+    a = parseInt(prompt('Ingrese un numero'));
+    b = parseInt(prompt('Ingrese otro numero'));
+    restar(a, b);
+    //TODO ver sugerencia VSCode restar()
+    opt = prompt('¿Desea realizar otra resta? (SI/NO)');
+}
